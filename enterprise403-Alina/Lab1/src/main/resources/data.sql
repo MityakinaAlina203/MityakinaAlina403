f76ebd89453ff9aea12e462d981773eaee3ed9cf
MityakinaAlina203 <145194259+MityakinaAlina203@users.noreply.github.com>
2026-02-19 21:20:19 +0300

    Delete enterprise403-Alina/Lab1 directory

--- enterprise403-Alina/Lab1/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customer (first_name, last_name, email, created_at) VALUES
-                                                                     ('Виктор', 'Самойлов', 'viktorsam@example.com', NOW()),
-                                                                     ('Елена', 'Тихомирова', 'elenatix@example.com', NOW());
